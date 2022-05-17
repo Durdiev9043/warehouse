0c2bcd895d6b415946517141e602879ed0e8e570
durdiev9043 <donyorbek9043@gmail.com>
2022-05-17 10:51:30 +0500

    first commit

---
+++ app/Http/Controllers/Admin/RoleController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    public function index()
+    {
+        $roles=Role::whereNotIn('name', ['Admin'])->get();
+        return view('admin.roles.index',['roles'=>$roles]);
+    }
+
+    public function create(){
+        return view('admin.roles.create');
+    }
+
+    public function store(Request $request){
+        $validated=$request->validate(['name'=>'required']);
+        Role::create($validated);
+
+        return to_route('admin.roles.index');
+    }
+    public function edit(Role $role)
+    {
+        return view('admin.roles.edit', ['role' => $role]);
+    }
+
+
+    public function update(Request $request, Role $role)
+    {
+        $validated=$request->validate(['name' => 'required']);
+        $role->update($validated);
+        return to_route('admin.roles.index');
+    }
+
+}
